9817993a65cc8bd6582cdc1dd2b165785d6576e8
nyh <nyh1006@163.com>
2013-09-11 13:41:06 +0800

    Initial Commit

---
+++ TheWeatherReport/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TheWeatherReport
+//
+//  Created by ios on 13-9-11.
+//  Copyright (c) 2013年 hxhd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
